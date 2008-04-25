36c130f4c35eb7d328bacc47a3ab077635ad89b9
peter_silva <peter_silva@de8836e2-9249-0410-b7f7-9a789b56f7ad>
2008-04-25 05:36:27 +0000

    Debian packaging stuff added.  now only works if installed in systemish places.

---
+++ trunk/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the droboutils package
+#
+0 4	* * *	root	droboutils_maintenance
---
+++ trunk/debian/droboutils-default.ex
@@ -0,0 +1,10 @@
+# Defaults for droboutils initscript
+# sourced by /etc/init.d/droboutils
+# installed at /etc/default/droboutils by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
