5584e92b45f515affc2c3b6dd55f6dad486b012e
Chris AtLee <catlee@sourceforge.net>
2008-09-15 14:09:34 +0000

    Fix up debian packaging, most lintian warnings are now gone, we now use pysupport instead of pycentral, and we build the .so for all supported versions of python.

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the droboutils package
-#
-0 4	* * *	root	droboutils_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(droboutils):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="droboutils" command="/usr/bin/droboutils"
