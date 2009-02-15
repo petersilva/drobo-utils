82124deaafcc947297a25c312a825203b5ca89d8
Peter Silva <peter_silva@sourceforge.net>
2009-02-15 14:06:58 +0000

    added 2009 to copyright notices. other fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Drobo Utils Copyright (C) 2008  Peter Silva (Peter.A.Silva@gmail.com)
+# Drobo Utils Copyright (C) 2008,2009  Peter Silva (Peter.A.Silva@gmail.com)
 # Drobo Utils comes with ABSOLUTELY NO WARRANTY; For details type see the file
 # named COPYING in the root of the source directory tree.
 
